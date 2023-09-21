b76a8fbfeaf7f42dc6a42fc7ae33181f799deb9a
tazkiyarvn <143949792+tazkiyarvn@users.noreply.github.com>
2023-09-21 20:43:13 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,47 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache = NULL
+  setMatrix <- function(matrix){
+    cache$matrix <<- matrix
+    cache$inverse <<- NULL
+  }
+  getMatrix <- function() {
+    cache$matrix
+  }
+  setInverse <- function(inv) cache <<- inv
+  getInverse <- function(){
+    if(!is.null(cache$inverse)){
+      message("Getting cached inverse")
+      return(cache$inverse)
+    }else{
+      message("Calculating and caching inverse")
+      inv <- solve(cache$matrix)
+      cache$inverse <- inv
+      return(inv)
+    }
+  }
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(cacheMatrix) {
         ## Return a matrix that is the inverse of 'x'
+  if (is.null(cacheMatrix$getMatrix())){
+    stop("Matrix is not set. Use setMatrix() first")
+  }
+  
+  cachedInverse <- cacheMatrix$getInverse()
+  
+  if (!is.null(cachedInverse)){
+    return(cachedInverse)
+  } else{
+    matrix <- cacheMatrix$getMatrix()
+    inv <- solve(matrix)
+    cacheMatrix$setMatrix(matrix)
+    cacheMatrix$getInverse()
+  }
 }
